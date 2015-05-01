394bfc664b6f52a3968343362226b8ca195fc7dc
Lars Haugan <lars@larshaugan.net>
2015-05-01 16:52:33 +0000

    Added host field and fixed autonaming

--- lib/stack.py
+++ lib/stack.py
@@ -12,20 +12,23 @@ class Stack(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     host = Column(String)
+    image = Column(String)
     service_id = Column(Integer, ForeignKey('service.id'))
     container = relationship('Container', backref=backref('stack'),
                              order_by='Container.id')
 
-    def __init__(self, name, service, host=None):
+    def __init__(self, service, image, host=None):
         """Creates a stack from a service
         Arguments:
-            name: Name of stack
             service: Parent service object
             host: Machine the stack exists on
+        Sets:
+            name: service name and len of stack
         """
-        self.name = name
         self.service = service
+        self.name = '%s-stack%s' % (self.service.name, len(self.service.stacks))
         self.host = host
+        self.image = image
 
     def get_state(self):
         state = {}
