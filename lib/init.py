de40ee4d05fdb684db50cd574d427f317159cc2e
Lars Haugan <lars@larshaugan.net>
2015-05-01 17:13:19 +0000

    Updated: initates two base applications for testing

---
+++ lib/init.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from os import path
+
+import base
+import config as cfg
+from service import Service
+from stack import Stack
+from container import Container
+from endpoint import Endpoint
+from stage import Stage
+from tree import Tree
+
+logging = cfg.get_logger()
+config = cfg.get_config()
+
+def init(create=False):
+    Base = base.Base
+
+    engine = create_engine(config.get('database','engine'),
+                           echo=config.getboolean('database','echo'))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    if create:
+        Base.metadata.create_all(engine)
+    return session
+
+
+def base_test_data(session, servicename, port):
+    s = Service(servicename, port)
+    for i in range(1,3):
+        webstack = Stack(s, 'image')
+        session.add(s)
+        session.add(webstack)
+        for i in range(1, 10):
+            c = Container(webstack, 'v0.%s' % i)
+            session.add(c)
+        session.commit()
+
+    session.commit()
+
+    s = session.query(Service).filter(Service.name == servicename).first()
+    e = Endpoint('web', s)
+    session.add(e)
+    session.commit()
+
+
+def get_state(session, service):
+    s = session.query(Service).filter(Service.name == service).first()
+    print s
+    if s:
+        endpoint = s.endpoints
+        print endpoint
+        print s.name
+        for stack in s.stacks:
+            for container in stack.container:
+                print container.name
+
+
+def main():
+    session = init(create=True)
+    base_test_data(session, 'WebFront', 10000)
+    base_test_data(session, 'webapp1', 10001)
+
+    get_state(session, 'WebFront')
+    get_state(session, 'webapp1')
+
+if __name__ == '__main__':
+    main()
