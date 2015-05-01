87f690f320e496c67fd563433b816c451e5d551a
Lars Haugan <lars@larshaugan.net>
2015-05-01 17:12:17 +0000

    Cleaned up and added parent child to state definition

--- lib/service.py
+++ lib/service.py
@@ -2,11 +2,9 @@
 # -*- coding: utf-8 -*-
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, text
 from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
 
 import base
 Base = base.Base
-#Base = declarative_base()
 
 parents = Table(
     'parents_rel', base.Base.metadata,
@@ -18,19 +16,17 @@ parents = Table(
 
 class Service(Base):
     __tablename__ = 'service'
+
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True)
     port = Column(Integer, nullable=False, unique=True)
-    # parent services ?
-    #parent_id = Column(Integer, ForeignKey('service.id'))
     childs = relationship('Service', secondary=parents,
-                      primaryjoin=id == parents.c.parent_id,
-                      secondaryjoin=id == parents.c.service_id, backref='parents')
+                          primaryjoin=id == parents.c.parent_id,
+                          secondaryjoin=id == parents.c.service_id,
+                          backref='parents')
 
-    # child services ?gg
     stacks = relationship('Stack', backref=backref('service', order_by=id))
     endpoints = relationship('Endpoint', backref=backref('service'))
-    #tree
 
     def __init__(self, name, port):
         self.name = name
@@ -40,17 +36,12 @@ class Service(Base):
         state = {}
         state['name'] = self.name
         state['port'] = self.port
-        #state['stacks'] = [s.get_state() for s in self.stacks]
         state['stacks'] = []
         for stack in self.stacks:
             state['stacks'].append(stack.get_state())
+        state['parent'] = [parent.name for parent in self.parents]
+        state['childs'] = [child.name for child in self.childs]
         return state
-
-    #@property
-    #def name(self): return self.name
-
-    #@name.setter
-    #def name(self, name): self.name = name
 
 
 if __name__ == '__main__':
