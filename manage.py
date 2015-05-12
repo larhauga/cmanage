ab5b4755f578a82c42619688eac4af5360765b6c
Lars Haugan <lars@larshaugan.net>
2015-05-12 16:35:33 +0000

    Fixed to pep8

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Global imports
 import argparse
-from sqlalchemy.exc import IntegrityError
 import requests
+from sqlalchemy.exc import IntegrityError
 
-#from lib import init
+# Local imports
 import basefunc
 from lib import init
 from lib import config as cfg
@@ -13,17 +14,16 @@ config = cfg.get_config()
 rules = cfg.get_rules_config()
 cconfig = cfg.get_container_config()
 
+
 def create_db(args):
+    """Initiates based on config"""
     init.init(create=True)
 
-def new_service(args): #name, image, versions, nrstacks=1):
+
+def new_service(args):
     """Process function for deploying service
     Arguments:
         args: name, image, versions, stacks
-            #name: The name of the service
-            #image: Docker image (repo)
-            #versions: list of versions or one version ['version']
-            stacks: number of stacks
     """
     service = basefunc.create_service(args.name)
     if not service:
@@ -39,6 +39,7 @@ def new_service(args): #name, image, versions, nrstacks=1):
         except StandardError as e:
             print e
 
+
 def new_endpoint(args):
     """Creates a new endpoint connected to a service"""
     service = basefunc.get_service(args.service)
@@ -50,6 +51,7 @@ def new_endpoint(args):
         except IntegrityError as e:
             print "Endpoint not added: %s" % (e.message)
             basefunc.session.rollback()
+
 
 def connect(args):
     """Connect two or more services together"""
@@ -75,17 +77,11 @@ def connect(args):
         print "Endpoint (%s) or service (%s) not found" % (args.endpoint,
                                                            args.service)
 
-    #create_tree
 
 def new_version(args):
-    """New version of container"""
-    # push + pop
-    # service
-    # for stack in service
-    # new version
-    # pop one?
-    # args.service
-    # args.version
+    """New version of container
+    Finds service, pushes new containers
+    """
     service = basefunc.get_service(args.service)
     containers = []
     for stack in service.stacks:
@@ -110,9 +106,8 @@ def pop(args):
         else:
             basefunc.pop(service, None, position=args.stackpointer)
 
+
 def view(args):
-    #if 'help' in args.name:
-        #print "Show {all, service(s), endpoint(s), stack(s), tree(service)}"
     # show all services
     if 'services' in args.type:
         basefunc.view_services()
@@ -125,13 +120,12 @@ def view(args):
     # Show one endpoint
     elif 'endpoint' in args.type and args.name:
         basefunc.view_endpoint(args.name)
-    #elif 'stacks' in args.type:
-        #basefunc.view_stack
     elif 'containers' in args.type:
         basefunc.view_containers()
     elif 'stack' in args.type and args.name:
         service = basefunc.get_service(args.name)
         basefunc.view_stack(service)
+
 
 def delete(args):
     if 'containers' in args.type:
@@ -139,9 +133,11 @@ def delete(args):
     else:
         print "Not running. Argument %s not supported" % (args.type)
 
+
 def deploy(args):
     if 'containers' in args.type:
         basefunc.deploy_all_containers()
+
 
 def upstream(args):
     """Get the upstream versions of a container"""
@@ -159,66 +155,85 @@ def upstream(args):
     else:
         print "Service not found"
 
-def main(args):
-    pass
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Service management tool',
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = argparse.ArgumentParser(
+        description='Service management tool',
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
 
     subparser = parser.add_subparsers()
-    parser_init = subparser.add_parser('init', description='Setup program for first time use')
+    # When init is sendt
+    parser_init = subparser.add_parser('init', description='Initiates database')
     parser_init.set_defaults(func=create_db)
-    # Parser for the view command
+
+    # Parsing for show command
     parser_view = subparser.add_parser('show', description='Show the different items')
-    parser_view.add_argument('type', type=str, choices=['services', 'service',
-                                                       'endpoints', 'endpoint',
-                                                       'stacks', 'stack',
-                                                        'containers'],
-                             help='Different types of items to show')
-    #parser_view.add_argument('-n', '--name', type=str,
+    parser_view.add_argument('type', type=str,
+                             help='Different types of items to show',
+                             choices=['services', 'service', 'endpoints',
+                            'endpoint', 'stacks', 'stack', 'containers'])
     parser_view.add_argument('name', type=str, nargs='?',
                              help='Name of service. Use percent to search')
     parser_view.set_defaults(func=view)
 
     # Parser for the addservice command
-    parser_add_service = subparser.add_parser('addservice', description="Add new service")
+    parser_add_service = subparser.add_parser('addservice',
+                                              description="Add new service")
     parser_add_service.add_argument('name', type=str, help='Name of service')
-    parser_add_service.add_argument('-i', '--image', type=str, help='Docker image path for pull')
-    parser_add_service.add_argument('-s', '--stacks', type=int, default=1, help='Number of stacks to add')
-    parser_add_service.add_argument('-v', '--versions', nargs='+', type=str, help='Version(s) to add')
+    parser_add_service.add_argument('-i', '--image', type=str,
+                                    help='Docker image path for pull')
+    parser_add_service.add_argument('-s', '--stacks', type=int, default=1,
+                                    help='Number of stacks to add')
+    parser_add_service.add_argument('-v', '--versions', nargs='+', type=str,
+                                    help='Version(s) to add')
     parser_add_service.set_defaults(func=new_service)
 
-    parser_add_endpoint = subparser.add_parser('addendpoint', description='Add a new endpoint to service')
-    parser_add_endpoint.add_argument('-s', '--service', type=str, help='Name of the main service', required=True)
+    parser_add_endpoint = subparser.add_parser('addendpoint',
+                                    description='Add a new endpoint to service')
+    parser_add_endpoint.add_argument('-s', '--service', type=str, required=True,
+                                     help='Name of the main service', )
     parser_add_endpoint.add_argument('name', type=str, help='Name of endpoint')
     parser_add_endpoint.add_argument('-p', '--port', help='Public port')
     parser_add_endpoint.add_argument('-stack', '--stackpointer', type=int, default=0,
                                      help='Default stack pointer for tree')
     parser_add_endpoint.set_defaults(func=new_endpoint)
 
-    parser_connect = subparser.add_parser('connect', description='Connect services')
+    parser_connect = subparser.add_parser('connect',
+                                          description='Connect services')
     parser_connect.add_argument('service', type=str, help='Service to connect')
-    parser_connect.add_argument('-p', '--parent', nargs='+', type=str, help='Parent service')
-    parser_connect.add_argument('-c', '--child', nargs='+', type=str, help='Childe service')
-    parser_connect.add_argument('-e', '--endpoint', type=str, help='Endpoint')
-    parser_connect.add_argument('-stp', '--stackpointer', type=int, help='Optional stack pointer')
+    parser_connect.add_argument('-p', '--parent', nargs='+', type=str,
+                                help='Parent service')
+    parser_connect.add_argument('-c', '--child', nargs='+', type=str,
+                                help='Childe service')
+    parser_connect.add_argument('-e', '--endpoint', type=str,
+                                help='Endpoint')
+    parser_connect.add_argument('-stp', '--stackpointer', type=int,
+                                help='Optional stack pointer')
     parser_connect.set_defaults(func=connect)
 
 
-    parser_do_release = subparser.add_parser('release', description='Do a service release')
-    parser_do_release.add_argument('service', type=str, help='Service to do release on')
-    parser_do_release.add_argument('-v', '--version', type=str, required=True, help='Version of the new container')
+    parser_do_release = subparser.add_parser('release',
+                                             description='Do a service release')
+    parser_do_release.add_argument('service', type=str,
+                                   help='Service to do release on')
+    parser_do_release.add_argument('-v', '--version', type=str, required=True,
+                                   help='Version of the new container')
     parser_do_release.set_defaults(func=new_version)
 
-    parser_pop = subparser.add_parser('pop', description='Pop container from service')
-    parser_pop.add_argument('service', type=str, help='Service to pop container from')
-    parser_pop.add_argument('-stp', '--stackpointer', type=int, default=0, help='Which container to pop')
-    parser_pop.add_argument('-s', '--single', default=False, action='store_false', help='Remove from one stacks')
+    parser_pop = subparser.add_parser('pop',
+                                      description='Pop container from service')
+    parser_pop.add_argument('service', type=str,
+                            help='Service to pop container from')
+    parser_pop.add_argument('-stp', '--stackpointer', type=int, default=0,
+                            help='Which container to pop')
+    parser_pop.add_argument('-s', '--single', default=False, action='store_false',
+                            help='Remove from one stacks')
     parser_pop.set_defaults(func=pop)
 
     parser_delete = subparser.add_parser('delete', description='Delete')
-    parser_delete.add_argument('type', type=str, choices=['containers', 'services', 'stacks'])
+    parser_delete.add_argument('type', type=str, choices=['containers',
+                                                          'services', 'stacks'])
     parser_delete.add_argument('-n', '--name', type=str,
                                help='Name of item to remove')
     parser_delete.set_defaults(func=delete)
@@ -229,11 +244,13 @@ if __name__ == '__main__':
                                help='Name of item to deploy')
     parser_deploy.set_defaults(func=deploy)
 
-    parser_upversions = subparser.add_parser('upstream', description='Find upstream versions')
-    parser_upversions.add_argument('service', type=str, help='The version to check versions on')
+    parser_upversions = subparser.add_parser('upstream',
+                                             description='Find upstream versions')
+    parser_upversions.add_argument('service', type=str,
+                                   help='The version to check versions on')
     parser_upversions.set_defaults(func=upstream)
     # Extra parameters for push on stack
-    #parser.add_argument('-v', '--version', type=str,
+    # parser.add_argument('-v', '--version', type=str,
                         #help='Versions of new containers. If containers > 1, this must be a list')
 
     args = parser.parse_args()
